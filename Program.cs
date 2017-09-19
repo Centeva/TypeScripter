8a13d98267ad8ae3669370f79fbf3ae842620204
AsaBeckstead <Asa.Beckstead@Centeva.com>
2017-09-19 10:15:10 -0600

    Add derived types to be typescripted.

--- Program.cs
+++ Program.cs
@@ -6,20 +6,16 @@ using System.Linq;
 using System.Reflection;
 using TypeScripter.Generators;
 
-namespace TypeScripter
-{
-	class Program
-	{
+namespace TypeScripter {
+	class Program {
 		private static string _basePath;
 		private static Options _options;
 
-		static int Main(string[] args)
-		{
+		static int Main(string[] args) {
 			Stopwatch sw = Stopwatch.StartNew();
 
 			int loadResult = LoadOptions(args);
-			if (loadResult != 0)
-			{
+			if(loadResult != 0) {
 				return loadResult;
 			}
 
@@ -39,11 +35,10 @@ namespace TypeScripter
 
 			// Get all types that are returned from an API call or are in the parameter list
 			var topLevelModels = new HashSet<Type>(apiControllers.SelectMany(GetModelsFromController));
-			var allModels = new HashSet<Type>(topLevelModels);
+			var allModels = new HashSet<Type>(GetDerivedTypes(topLevelModels));// Add derived types
 
 			// Now recursively search the top level models for child models
-			foreach (var model in allModels.ToArray())
-			{
+			foreach(var model in allModels.ToArray()) {
 				RecursivelySearchModels(model, allModels);
 			}
 			Console.WriteLine("Found {0}", allModels.Count);
@@ -53,144 +48,104 @@ namespace TypeScripter
 				EntityGenerator.Generate(targetPath, allModels),
 				DataServiceGenerator.Generate(_options.ApiRelativePath, apiControllers, topLevelModels, targetPath)
 			);
-			
+
 			RemoveNonGeneratedFiles(targetPath, allGeneratedNames);
 
 			Console.WriteLine("Done in {0:N3}s", sw.Elapsed.TotalSeconds);
 			return 0;
 		}
 
-		private static int LoadOptions(string[] args)
-		{
-			if (args.Length != 1 && args.Length > 3)
-			{
+		private static int LoadOptions(string[] args) {
+			if(args.Length != 1 && args.Length > 3) {
 				Console.WriteLine("Usage: typescripter.exe <Options File Path>");
 				Console.WriteLine("       typescripter.exe <dll source path> <model target path> <api relative path (optional)>");
 				Console.WriteLine("Note:  If the <api relative path> is provided, either in the command line or options file, TypeScripter will generate a DataService.ts file.");
 				Console.WriteLine("Example: typescripter.exe \"Options.json\"");
 				Console.WriteLine("         typescripter.exe \"c:\\Source\\Project\\bin\\debug\" \"c:\\Source\\Project\\src\\app\\models\\generated\"");
 				Console.WriteLine("------------------------------------");
-				for (var i = 0; i < args.Length; i++)
-				{
+				for(var i = 0; i < args.Length; i++) {
 					Console.WriteLine("[{0}] = {1}", i, args[i]);
 				}
 				return 1;
 			}
 
-			if (args.Length == 1)
-			{
-				if (File.Exists(args[0]))
-				{
-					try
-					{
+			if(args.Length == 1) {
+				if(File.Exists(args[0])) {
+					try {
 						_options = FileHandler.ReadJson<Options>(args[0]);
-						if (string.IsNullOrEmpty(_options.Source))
-						{
+						if(string.IsNullOrEmpty(_options.Source)) {
 							throw new Exception("Source is null or empty in options.");
 						}
-						if (string.IsNullOrEmpty(_options.Destination))
-						{
+						if(string.IsNullOrEmpty(_options.Destination)) {
 							throw new Exception("Destination is null or empty in options.");
 						}
-						if (_options.Files == null || _options.Files.Length <= 0)
-						{
-							_options.Files = new[] {"*.client.dll"};
+						if(_options.Files == null || _options.Files.Length <= 0) {
+							_options.Files = new[] { "*.client.dll" };
 						}
-						if (_options.ControllerBaseClassNames == null || _options.ControllerBaseClassNames.Length <= 0)
-						{
-							_options.ControllerBaseClassNames = new[] {"ApiController"};
+						if(_options.ControllerBaseClassNames == null || _options.ControllerBaseClassNames.Length <= 0) {
+							_options.ControllerBaseClassNames = new[] { "ApiController" };
 						}
 					}
-					catch (Exception e)
-					{
+					catch(Exception e) {
 						Console.WriteLine(e.Message);
-						{
-							return 2;
-						}
+						return 2;
 					}
 				}
-				else
-				{
+				else {
 					Console.WriteLine("No options file found.");
-					{
-						return 3;
-					}
+					return 3;
 				}
 			}
-			else if (args.Length >= 2)
-			{
-				_options = new Options
-				{
+			else if(args.Length >= 2) {
+				_options = new Options {
 					Source = args[0],
 					Destination = args[1],
-					Files = new[] {"*.client.dll"},
-					ControllerBaseClassNames = new[] {"ApiController"}
+					Files = new[] { "*.client.dll" },
+					ControllerBaseClassNames = new[] { "ApiController" }
 				};
-				if (args.Length >= 3)
-				{
+				if(args.Length >= 3) {
 					_options.ApiRelativePath = args[2];
 				}
 			}
 			return 0;
 		}
 
-		private static Assembly ResolveAssembly(object sender, ResolveEventArgs args)
-		{
-			try
-			{
-				string file = Path.Combine(_basePath, args.Name.Substring(0, args.Name.IndexOf(",", StringComparison.Ordinal)) + ".dll");
+		private static Assembly ResolveAssembly(object sender, ResolveEventArgs args) {
+			try {
+				var file = Path.Combine(_basePath, args.Name.Substring(0, args.Name.IndexOf(",", StringComparison.Ordinal)) + ".dll");
 				return Assembly.LoadFile(file);
 			}
-			catch
-			{
+			catch {
 				Console.WriteLine(args.Name);
 				return null;
 			}
 		}
 
-		private static Assembly Load(string path)
-		{
-			try
-			{
-				return Assembly.LoadFile(path);
-			}
-			catch
-			{
-				return null;
-			}
+		private static Assembly Load(string path) {
+			try { return Assembly.LoadFile(path); }
+			catch { return null; }
 		}
 
-		private static string AbsolutePath(string relativePath)
-		{
+		private static string AbsolutePath(string relativePath) {
 			return Path.IsPathRooted(relativePath) ? relativePath : Path.Combine(Environment.CurrentDirectory, relativePath);
 		}
 
-		private static IEnumerable<Type> GetApiControllers(Assembly a)
-		{
+		private static IEnumerable<Type> GetApiControllers(Assembly a) {
 			return TryGetApiControllerTypes(a).Where(t => t.BaseType != null && _options.ControllerBaseClassNames.Contains(t.BaseType.Name));
 		}
 
-		private static IEnumerable<Type> TryGetApiControllerTypes(Assembly a)
-		{
-			try
-			{
-				return a.GetTypes();
-			}
-			catch
-			{
-				return Enumerable.Empty<Type>();
-			}
+		private static IEnumerable<Type> TryGetApiControllerTypes(Assembly a) {
+			try { return a.GetTypes(); }
+			catch { return Enumerable.Empty<Type>(); }
 		}
 
-		private static IEnumerable<Type> GetModelsFromController(Type controllerType)
-		{
+		private static IEnumerable<Type> GetModelsFromController(Type controllerType) {
 			var methods = controllerType.GetMethods();
 			var models = methods.SelectMany(GetModelsFromMethod);
 			return models;
 		}
 
-		private static IEnumerable<Type> GetModelsFromMethod(MethodInfo arg)
-		{
+		private static IEnumerable<Type> GetModelsFromMethod(MethodInfo arg) {
 			return GetModelTypes(arg.ReturnType)
 				.Union(arg.GetParameters()
 					.Select(p => p.ParameterType)
@@ -198,58 +153,55 @@ namespace TypeScripter
 				);
 		}
 
-		private static IEnumerable<Type> GetModelTypes(Type t)
-		{
-			if (t.GetCustomAttributes().Any(x => x.GetType().Name == "TypeScripterIgnoreAttribute")) yield break;
-			if (t.IsModelType())
-			{
-				if (t.IsArray)
-				{
+		private static IEnumerable<Type> GetDerivedTypes(IEnumerable<Type> types) {
+			var typesList = types.ToList();
+			var allTypes = new HashSet<Type>(typesList.ToList());
+			foreach(var type in typesList) {
+				allTypes.UnionWith(type.Assembly.GetTypes().Where(t => t != type && type.IsAssignableFrom(t)));
+			}
+			return allTypes;
+		}
+
+		private static IEnumerable<Type> GetModelTypes(Type t) {
+			if(t.GetCustomAttributes().Any(x => x.GetType().Name == "TypeScripterIgnoreAttribute")) yield break;
+			if(t.IsModelType()) {
+				if(t.IsArray) {
 					yield return t.GetElementType();
 				}
-				else
-				{
+				else {
 					yield return t;
 				}
 			}
-			else
-			{
-				if (t.IsGenericType)
-				{
-					foreach (var a in t.GetGenericArguments().Where(a => a.IsModelType()).SelectMany(GetModelTypes))
-					{
+			else {
+				if(t.IsGenericType) {
+					foreach(var a in t.GetGenericArguments().Where(a => a.IsModelType()).SelectMany(GetModelTypes)) {
 						yield return a;
 					}
 				}
 			}
-			if (t.BaseType != null && t.BaseType.IsModelType())
-			{
+			if(t.BaseType != null && t.BaseType.IsModelType()) {
 				yield return t.BaseType;
 			}
 		}
 
-		private static void RecursivelySearchModels(Type model, HashSet<Type> visitedModels)
-		{
+		private static void RecursivelySearchModels(Type model, HashSet<Type> visitedModels) {
 			var props = model
 				.GetProperties()
 				.Select(p => p.GetPropertyType()).SelectMany(GetModelTypes).Where(t => !visitedModels.Contains(t) && t.IsModelType());
-			foreach (var p in props)
-			{
+			foreach(var p in props) {
 				visitedModels.Add(p);
 				RecursivelySearchModels(p, visitedModels);
 			}
 		}
 
-		private static void RemoveNonGeneratedFiles(string targetPath, List<string> allGeneratedNames)
-		{
+		private static void RemoveNonGeneratedFiles(string targetPath, List<string> allGeneratedNames) {
 			// Make sure output directory does not contain any non-generated files that might cause problems
 			var filesToDelete = Directory
 				.GetFiles(targetPath)
 				.Select(Path.GetFileName)
 				.Except(allGeneratedNames.Select(m => m + ".ts"), StringComparer.OrdinalIgnoreCase);
 
-			foreach (var file in filesToDelete)
-			{
+			foreach(var file in filesToDelete) {
 				File.Delete(Path.Combine(targetPath, file));
 			}
 		}
