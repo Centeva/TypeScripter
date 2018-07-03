4be71714db3fc735cfc965b2bfe7919819ad4127
Travis Marble <travis.marble@centeva.com>
2018-07-03 13:14:13 -0600

    fix imports in tests.

--- TypeScripter.Tests/UtilsUnitTests.cs
+++ TypeScripter.Tests/UtilsUnitTests.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TypeScripter;
+using TypeScripter.Common;
 
 namespace TypeScripter.Tests
 {
