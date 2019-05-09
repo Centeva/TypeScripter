25b3ddc55f2e19e733b812f85a905d5d64b7b171
devyn cyphers <devyn.cyphers@centeva.com>
2019-05-09 15:37:39 -0600

    Add more info to fields

--- TypeScripter.Common/Generators/SchemaGenerator.cs
+++ TypeScripter.Common/Generators/SchemaGenerator.cs
@@ -4,9 +4,16 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using Newtonsoft.Json;
 using TypeScripter.Attributes;
+ 
+internal class SchemaFieldModel {
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
 
 internal class SchemaModel
 {
@@ -14,7 +21,7 @@ internal class SchemaModel
     public string Name { get; set; }
     public string QualifiedName { get; set; }
     
-    public List<Guid> Fields { get; set; }
+    public List<SchemaFieldModel> Fields { get; set; }
     public bool IsClass { get; set; }
     public string Description { get; set; }
 }
@@ -51,9 +58,16 @@ namespace TypeScripter.Common.Generators
                         QualifiedName = model.AssemblyQualifiedName,
                         Fields = memberList.Select(f =>
                         {
+                            var propAttribute = f.GetCustomAttribute<TypescripterDocAttribute>();
                             if(!typeLookup.ContainsKey(f.GUID))
                                 typeLookup.Add(f.GUID, f);
-                            return f.GUID;
+
+                            return new SchemaFieldModel
+                            {
+                                Id = f.GUID,
+                                Name = f.Name,
+                                Description = propAttribute?.Description
+                            };
                         }).ToList(),
                         Description = docAttribute?.Description,
                         IsClass = model.IsClass
@@ -71,11 +85,11 @@ namespace TypeScripter.Common.Generators
             {
                 if(count > 100)
                     throw new Exception("ERROR: Schema generator is caught in a loop!");
-                var missingFields = new HashSet<Guid>(schema.SelectMany(s => s.Fields).Where(fId => schema.All(l => l.Id != fId)));
-                var missingTypes = typeLookup.Where(t => missingFields.Contains(t.Key));
+                var missingFields = new HashSet<SchemaFieldModel>(schema.SelectMany(s => s.Fields).Where(f => schema.All(l => l.Id != f.Id)));
+                var missingTypes = typeLookup.Where(t => missingFields.All(f => f.Id == t.Key));
                 schema.AddRange(GetTypes(new HashSet<Type>(missingTypes.Select(t => t.Value))));
                 count++;
-                check = schema.SelectMany(s => s.Fields).Any(fId => schema.All(s => s.Id != fId));
+                check = schema.SelectMany(s => s.Fields).Any(f => schema.All(s => s.Id != f.Id));
             }
 
             var json = JsonConvert.SerializeObject(schema);
